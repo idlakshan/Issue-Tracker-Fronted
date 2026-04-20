09b15de6339ecbb0dfe236e14b0e5fb0d0a0beb9
idlakshan <id.lakshan21@gmail.com>
2026-04-20 15:04:54 +0530

    feat: add input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Download } from "lucide-react";
+import { Download, Search } from "lucide-react";
 import "./App.css";
 import Button from "./components/ui/button";
+import Input from "./components/ui/input";
 
 function App() {
   return (
     <div className="p-4 space-x-4">
       <Button variant="primary">Primary</Button>
       <Button variant="secondary" icon={<Download />} />
+
+      <Input placeholder="Enter your name" icon={<Search />} />
     </div>
   );
 }
