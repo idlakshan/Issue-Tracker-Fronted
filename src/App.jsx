0047df21a52f59e5397f5c32ee317114ebac5af9
idlakshan <id.lakshan21@gmail.com>
2026-04-21 03:38:32 +0530

    feat: protect admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import AppLayout from "./components/layout/app-layout";
 import Dashboard from "./pages/dashboard";
 import AllIssues from "./pages/all-issue";
+import ProtectedRoute from "./components/layout/protected-layout";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route element={<AppLayout />}>
-        <Route path="/admin" element={<Dashboard />} />
-        <Route path="/admin/issues" element={<AllIssues />} />
-        <Route path="/admin/assignees" element={<div>Admin Assignees</div>} />
+      <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
+        <Route element={<AppLayout />}>
+          <Route path="/admin" element={<Dashboard />} />
+          <Route path="/admin/issues" element={<AllIssues />} />
+          <Route path="/admin/assignees" element={<div>Assignees</div>} />
+        </Route>
       </Route>
     </Routes>
   );
---
+++ src/components/layout/protected-layout.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute = ({ allowedRoles }) => {
+  const { user, accessToken } = useSelector((state) => state.auth);
+
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!allowedRoles.includes(user.role)) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
