da09e1591b8082942a5664368ffb03de6130498f
idlakshan <id.lakshan21@gmail.com>
2026-04-21 19:43:02 +0530

    fix protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import AppLayout from "./components/layout/app-layout";
 import Dashboard from "./pages/dashboard";
 import AllIssues from "./pages/all-issue";
 import ProtectedRoute from "./components/layout/protected-layout";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { user, accessToken } = useSelector((state) => state.auth);
+  //console.log(user, accessToken);
+  
+  const getDefaultRoute = () => {
+    if (!accessToken || !user) return "/login";
+    return user.role === "ADMIN" ? "/admin" : "/dev";
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to={getDefaultRoute()} replace />} />
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={!accessToken ? <Login /> : <Navigate to={getDefaultRoute()} replace />} />
+      <Route path="/register" element={!accessToken ? <Register /> : <Navigate to={getDefaultRoute()} replace />} />
 
       <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
         <Route element={<AppLayout />}>
           <Route path="/admin" element={<Dashboard />} />
           <Route path="/admin/issues" element={<AllIssues />} />
-          <Route path="/admin/assignees" element={<div>Assignees</div>} />
         </Route>
       </Route>
+
+
+      <Route element={<ProtectedRoute allowedRoles={["DEV"]} />}>
+        <Route element={<AppLayout />}>
+          <Route path="/dev" element={<div>Developer Dashboard</div>} />
+          <Route path="/dev/my-tasks" element={<div>My Tasks</div>} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<Navigate to={getDefaultRoute()} replace />} />
     </Routes>
   );
 }
--- src/components/layout/protected-layout.jsx
+++ src/components/layout/protected-layout.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ allowedRoles }) => {
     return <Navigate to="/login" replace />;
   }
 
-  if (!allowedRoles.includes(user.role)) {
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
     return <Navigate to="/login" replace />;
   }
 
